6bfbbb4614c8d79e5361876c5b22230eae855f7e
Lari Hoppula <lari@hoppula.com>
2016-01-29 07:55:54 +0200

    Use react-router 2 & react-router-redux 2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
 import matchRoutes from 'react-router/lib/matchRoutes'
 import { createRoutes } from 'react-router'
+import { UPDATE_LOCATION } from 'react-router-redux'
+export const UPDATE_LOCATION_WITH_PARAMS = "@@router/UPDATE_LOCATION_WITH_PARAMS"
 
-// declaring INIT_PATH here as redux-simple-router doesn't export it for some reason
-const INIT_PATH = '@@router/INIT_PATH'
-import { UPDATE_PATH } from 'redux-simple-router'
-export const UPDATE_PATH_EXTENDED = "@@router/UPDATE_PATH_EXTENDED"
-
-function updateWithParamsAndQuery(payload) {
+function updateWithParams(payload) {
   return {
-    type: UPDATE_PATH_EXTENDED,
+    type: UPDATE_LOCATION_WITH_PARAMS,
     payload
   }
 }
 
-export function paramsAndQueryMiddleware(routes, history) {
-  const { createLocation } = history
+export function paramsMiddleware(routes, history) {
   const routesArray = createRoutes(routes)
 
   return store => next => action => {
-    if (action.type === INIT_PATH || action.type === UPDATE_PATH) {
-      const location = createLocation(action.payload.path)
-      matchRoutes(routesArray, location, (error, state) => {
+    if (action.type === UPDATE_LOCATION) {
+      matchRoutes(routesArray, action.payload, (error, state) => {
         if (!error) {
           store.dispatch(
-            updateWithParamsAndQuery({
-              path: action.payload.path,
-              avoidRouterUpdate: action.payload.avoidRouterUpdate,
-              state: action.payload.state,
-              replace: action.payload.replace,
-              params: state.params,
-              query: location.query
+            updateWithParams({
+              location: action.payload,
+              params: state.params
             })
           )
         }
@@ -41,27 +32,25 @@ export function paramsAndQueryMiddleware(routes, history) {
 }
 
 const initialState = {
-  changeId: 1,
-  path: undefined,
-  state: undefined,
-  replace: false,
-  params: undefined,
-  query: undefined
+  location: undefined,
+  params: {}
 }
 
-function update(state = initialState, { type, payload }) {
-  if (type === UPDATE_PATH_EXTENDED) {
+export function routeReducer(state = initialState, { type, payload }) {
+  // support initial UPDATE_LOCATION that middleware does not catch for some reason
+  // TODO: investigate why
+  if (type === UPDATE_LOCATION) {
     return {
       ...state,
-      path: payload.path,
-      changeId: state.changeId + (payload.avoidRouterUpdate ? 0 : 1),
-      state: payload.state,
-      replace: payload.replace,
-      params: payload.params,
-      query: payload.query
+      location: payload
+    }
+  }
+  if (type === UPDATE_LOCATION_WITH_PARAMS) {
+    return {
+      ...state,
+      location: payload.location,
+      params: payload.params
     }
   }
   return state
 }
-
-export { update as routeReducer }
